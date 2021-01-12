a61c77d73563bbe8ed8d93a140e18f923d40550f
thefithagreement <mickael.vitry@gmail.com>
2021-01-12 10:17:04 +0100

    project init

---
+++ my_velov_assistant/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from my_velov_assistant.users.models import User
+from my_velov_assistant.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
