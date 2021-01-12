af3c4f47a0af0469b272450139f59bc785d0d21f
thefithagreement <mickael.vitry@gmail.com>
2021-01-12 18:24:40 +0100

    get location

--- my_velov_assistant/static/js/project.js
+++ my_velov_assistant/static/js/project.js
@@ -1,1 +1,21 @@
 /* Project specific Javascript goes here. */
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        document.getElementById("latitude").innerHTML = `latitude:${pos.lat}`
+        document.getElementById("longitude").innerHTML = `longitude:${pos.lng}`
+      },
+      () => {
+        console.log("Error: Your browser doesn't support geolocation.")
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    console.log("Error: The Geolocation service failed.")
+  }
+}
