de97b0749f68cc3bd1cfefd978e9397ecb209d0a
thefithagreement <mickael.vitry@gmail.com>
2021-01-13 14:45:14 +0100

    new tests api name and get station

--- my_velov_assistant/assistant/models.py
+++ my_velov_assistant/assistant/models.py
@@ -6,14 +6,14 @@ from django.db import models
 
 from my_velov_assistant.users.models import User
 
-API_KEY = os.environ["API_KEY"]
-API_URL = "https://api.jcdecaux.com/vls/v3/stations"
+DECAUX_API_KEY = os.environ["DECAUX_API_KEY"]
+DECAUX_API_URL = "https://api.jcdecaux.com/vls/v3/stations"
 
 
 def get_stations(city: str = "lyon") -> list:
     """Returns the list of the velov stations filtered by the city."""
 
-    url = f"{API_URL}?apiKey={API_KEY}"
+    url = f"{DECAUX_API_URL}?apiKey={DECAUX_API_KEY}"
     response = urllib.request.urlopen(url)
     stations = json.loads(response.read().decode())
 
@@ -24,8 +24,9 @@ def get_stations(city: str = "lyon") -> list:
 class ApiCall(models.Model):
     """Log the call to the Google Direction API"""
 
+    api_name = models.CharField(max_length=20, null=False, default="")
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     called_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
-        return f"Called at {self.called_at:%H:%M %Y-%m-%d} by {self.created_by}"
+        return f"{self.api_name} api called at {self.called_at:%H:%M %Y-%m-%d} by {self.created_by}"
--- my_velov_assistant/assistant/tests.py
+++ my_velov_assistant/assistant/tests.py
@@ -2,19 +2,31 @@ from django.test import TestCase
 
 from my_velov_assistant.users.tests.factories import UserFactory
 
-from .models import ApiCall
+from .models import ApiCall, get_stations
 
 
 # ApiCall
 class ApiCallTestCase(TestCase):
     def test_new_call_is_registered(self):
         api_call_count = ApiCall.objects.count()
+        # create a fake user
         user = UserFactory()
         ApiCall.objects.create(created_by=user)
         self.assertEqual(ApiCall.objects.count(), api_call_count + 1)
 
+    def test_new_call_api_name(self):
+        # create a fake user
+        user = UserFactory()
+        api_call = ApiCall.objects.create(api_name="test_api", created_by=user)
+        self.assertEqual(api_call.api_name, "test_api")
 
-# read the stations
+
+# Velov test cases
+class VelovTestCase(TestCase):
+    def test_get_station_is_not_empty(self):
+        velov_stations = get_stations()
+        self.assertNotEquals(len(velov_stations), 0)
+
 
 # distance from position
 
